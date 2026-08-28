95e223246ceda21da06f8fbbdcb7b267ba761d41
Knaggs <dylan.knaggs@gmail.com>
2026-08-28 14:51:45 -0500

    add tests and use bin_ages()

--- R/assemble-deaths.R
+++ R/assemble-deaths.R
@@ -5,12 +5,14 @@
 #' expectancy calculation
 #'
 #'
-#' @param deaths A 2 column data frame/ data frame extension (e.g. a tibble),
-#' with a column indicating age bins ("age_bin") and a column indicating death
-#' counts. The first row should be the age bin from 0-1.
+#' @param deaths A data frame/ data frame extension (e.g. a tibble),
+#' with at minimum a column indicating age bins ("age_bin") and a column
+#' indicating death counts. The first row should be the age bin from 0-1.
+#' @param age_col Name of the column with ages to be binned.
 #' @param ... Additional subgroups to be used for life expectancy calculation
-#' (e.g. race, geography, year)
-#' @param width age bin width to pass to make_age_bins()
+#' (e.g. race, geography, year).
+#' @param breaks either "lifex5" or "lifex10", indicating the age bin width to
+#' pass to bin_ages(). If not provided, will use "lifex10"
 #'
 #' @export
 #' @returns A data frame/data frame extension (e.g. a tibble) with counts of
@@ -21,20 +23,29 @@
 #'set.seed(0)
 #'n <- 100
 #'df <- data.frame(
-#'  AGE=sample(2:100,size=n,replace=TRUE),
-#'  SEX=sample(c("M","F"),size=n,replace=TRUE)
+#'  age=sample(2:100, size=n,replace=TRUE),
+#'  sex=sample(c("M","F"), size=n, replace=TRUE)
 #')
-#'assemble_deaths(df, width=10)
-#'assemble_deaths(df, SEX,width=5)
+#'assemble_deaths(deaths=df, age_col=age, breaks="lifex10")
+#'assemble_deaths(deaths=df, age_col=age, sex, breaks="lifex5")
 #'
 
 
-assemble_deaths <- function(deaths, ..., width = 10) {
+assemble_deaths <- function(deaths, age_col, ...,
+                            breaks = c("lifex5","lifex10")) {
 
-  deaths$age_bin <- make_age_bins(deaths$AGE, width = width)
+  if (missing(breaks)){
+    breaks <- "lifex10"
+  } else {
+    breaks <- match.arg(breaks)
+  }
+  print(breaks)
+
   deaths |>
-    count(age_bin, ...) |>
-    complete(age_bin, ...) |>
-    mutate(across(everything(), \(x) replace_na(x,0)))
-}
+    mutate(age_bin=bin_ages({{age_col}}, breaks = breaks)) |>
+    count(.data$age_bin, ...) |>
+    complete(.data$age_bin, ...) |>
+    mutate(n=replace_na(n,0)) |>
+    arrange(...,.data$age_bin)
 
+}
